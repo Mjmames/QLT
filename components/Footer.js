b4a992076a3be4fb5d802d6ecbe04f25509669b9
Mjmames <133544886+Mjmames@users.noreply.github.com>
2026-03-04 03:38:02 -0500

    Create Footer component for QLT ecosystem

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <p>© 2026 QLT Ecosystem. All Rights Reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;
